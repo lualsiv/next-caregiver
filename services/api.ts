c0d4af7b00d39886d7c07324ea35fa84c76158a3
MacPro <macpro@MacPros-Mac-Pro.local>
2021-02-08 16:45:46 -0300

    add baseURL

--- services/api.ts
+++ services/api.ts
@@ -1,13 +1,8 @@
 // api.js
 import Axios from "axios";
 
-let urls = {
-    test: `http://localhost:3334`,
-    development: 'http://localhost:3000/',
-    production: 'https://your-production-url.com/'
-}
 const api = Axios.create({
-    baseURL: urls[process.env.NODE_ENV],
+    baseURL: process.env.NEXTAUTH_URL,
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
